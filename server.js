4401567dc90a9fa22de5bc13412b9ae8d85747fd
ropeks <ropeksmilioner@gmail.com>
2019-07-25 12:11:43 +0200

    add cookies and sessions to login functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
 const Users = require('./data/users-model.js');
 
@@ -8,12 +9,23 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use(session({
+    name: 'sessionId',
+    secret: 'gfhghhjvcfxxgfcghvjjbjn',
+    cookie: {
+        maxAge: 1000 * 60 * 60,
+        secure: false,
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: true
+}));
 
 server.get('/', (req, res) => {
-    res.send('this thing is alive');
+    res.send('Welcome!');
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', restricted, (req, res) => {
     Users.getUsers()
         .then(users => {
             res.json(users);
@@ -43,6 +55,7 @@ server.post('/api/login', (req, res) => {
         .first()
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)) {
+                req.session.user = user;
                 res.status(200).json({ message: `Hello ${username}!` });
             } else {
                 res.status(401).json({ message: 'Invalid username or password' });
@@ -52,5 +65,13 @@ server.post('/api/login', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+function restricted(req, res, next) {
+    if (req.session && req.session.user) {
+        next();
+    } else {
+        res.status(400).json({ message: 'You are not logged in.' });
+    }
+}
 
 module.exports = server;
